6bab9d569cec3ecb096546e1e22350469cbc1257
HerlynMae <torresherlynmae@gmail.com>
2024-06-03 15:49:04 +0800

    no contact page

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: ".testimonial__slider",
+  items: 3,
+  gutter: 100,
+  slideBy: "page",
+  autoplay: true,
+  controls: false,
+  nav: false,
+  autoplayButtonOutput: false,
+});
