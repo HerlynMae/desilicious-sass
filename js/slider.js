16c76e4b4c6c9109de74c1812eaa74ff176d1a2c
HerlynMae <torresherlynmae@gmail.com>
2024-06-24 08:10:21 +0800

    w/ home responsive only

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,33 @@
+// var slider = tns({
+//   container: ".testimonial__slider",
+//   items: 3,
+//   gutter: 100,
+//   slideBy: "page",
+//   autoplay: true,
+//   controls: false,
+//   nav: false,
+//   autoplayButtonOutput: false,
+//   responsive: {
+//     1250: {
+//       items: 3,
+//     },
+//     200: {
+//       items: 1,
+//     },
+//   },
+// });
+
 var slider = tns({
   container: ".testimonial__slider",
   items: 3,
+  speed: 200,
   gutter: 100,
   slideBy: "page",
   autoplay: true,
-  controls: false,
-  nav: false,
-  autoplayButtonOutput: false,
+  navPosition: "bottom",
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
   responsive: {
     1250: {
       items: 3,
