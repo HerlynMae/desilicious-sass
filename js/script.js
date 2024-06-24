6292f212f47ec02ddccaaa2831c3884ab0dc686f
HerlynMae <torresherlynmae@gmail.com>
2024-06-24 12:28:23 +0800

    with scroll js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
+
+const headerScroll = document.querySelector(".header__wrapper");
+// console.log.window;
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    headerScroll.classList.add("open");
+  } else {
+    headerScroll.classList.remove("open");
+  }
+});
